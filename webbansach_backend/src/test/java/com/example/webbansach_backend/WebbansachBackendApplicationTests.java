c47d05601ab4e48f4e46da67eda28b181f98db9e
Trinh Duc Anh <trinhanh03444@gmail.com>
2024-11-30 19:22:18 +0700

    tao du an

---
+++ webbansach_backend/src/test/java/com/example/webbansach_backend/WebbansachBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webbansach_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbansachBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
